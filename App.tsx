99dc07a924254ea6a2638b04497772b6eb94fc16
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-28 18:37:52 -0300

    feat(mod1): tema global da aplicacao :hammer:

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Home } from './src/screens/Home';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { StatusBar } from 'react-native'
+import AppLoading from 'expo-app-loading'
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+
+import { Home } from './src/screens/Home'
+import theme from './src/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts( {
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium
+  } )
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <>
-      <StatusBar barStyle="light-content"/>
+    <ThemeProvider theme={theme} >
+      <StatusBar barStyle="light-content" />
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -4,5 +4,8 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: ${ ( { theme } ) => theme.colors.background_primary};
 `
-export const Title = styled.Text``
+export const Title = styled.Text`
+  font-family: ${ ( { theme } ) => theme.fonts.secundary_600};
+`
