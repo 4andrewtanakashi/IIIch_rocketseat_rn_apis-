ea4737604d70357f0a65981378db96391a3db13b
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-29 17:09:34 -0300

    feat(mod1): continuando a interface de detalhes :hammer:

--- src/screens/CardDetails/index.tsx
+++ src/screens/CardDetails/index.tsx
@@ -3,7 +3,16 @@ import React from 'react'
 import {
   Container,
   Header,
-  CarImageWarapper
+  CarImageWarapper,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider/'
@@ -20,6 +29,27 @@ export function CardDetails () {
           imageUrl={['https://www.evspecifications.info/wp-content/uploads/2020/01/Porsche-Panamera-4-e-hybrid-evchargeplus-00-1-1-1024x680.png']}
         />
       </CarImageWarapper>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo.
+          Surgiu do lendário touro de lide indultado
+          na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   )
 }
