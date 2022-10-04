6449659f62f2812751f574b91cffc141ab471b0d
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-10-04 08:40:30 -0300

    feat(mod1): exibindo carros agendados :hammer:

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from 'react'
+import { StatusBar, FlatList } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+import { useTheme } from 'styled-components'
 import  { CarDTO } from '../../dtos/CarDTO'
 import { api } from '../../services/api'
+import { BackButton } from '../../components/BackButton'
+import { Car } from '../../components/Car'
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuality
+} from './styles'
 
-import { Container } from './styles'
+interface CarProps {
+  id: string
+  user_id: string
+  car: CarDTO
+}
 
 export function MyCars () {
-  const [cars, setCars] = useState<CarDTO[]>([])
+  const [cars, setCars] = useState<CarProps[]>([])
   const [loading, setLoading] = useState(true)
+
+  const theme = useTheme()
+  const navigation = useNavigation()
+  const route = useRoute()
 
   useEffect( () => {
     async function fetchCars () {
       try {
-        const response = await api.get('/schedules_bycars?user_id=4')
+        const response = await api.get('/schedules_byuser?user_id=1')
+        console.log("response.data: ", response.data)
         setCars(response.data)
       } catch (error) {
         console.log(error as string)
@@ -22,9 +47,45 @@ export function MyCars () {
     fetchCars()
   }, [])
 
+  function handleBack () {
+    navigation.goBack()
+  }
+
   return (
     <Container>
+      <StatusBar 
+        barStyle={'light-content'}
+        translucent
+        backgroundColor={'transparent'}
+      />
+      <Header>
+        <BackButton
+          onPress={handleBack}
+          color={theme.colors.shape}  
+        />
 
+        <Title>
+          Seus agendamentos, {'\n'}
+          estão aqui.
+        </Title>
+        <SubTitle>Conforto, segurança e praticidade.</SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuality>05</AppointmentsQuality>
+        </Appointments>
+
+        <FlatList 
+          data={cars}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={ ({item}) => (
+            <Car data={item.car} />
+          ) }
+        />
+      </Content>
     </Container>
   )
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,54 @@
 import styled from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  background-color: ${ ({theme}) => theme.colors.background_primary};
+`
+export const Header = styled.View`
+  width: 100%;
+  height: 325px;
+  background: ${ ({theme}) => theme.colors.header };
+  justify-content: center;
+  padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`
+export const Title = styled.Text`
+  color: ${ ({theme}) => theme.colors.shape };
+  font-family: ${ ({theme}) => theme.fonts.secondary_600 };
+  font-size: ${RFValue(30)}px;
+  padding-top: 24px;
+`
+export const SubTitle = styled.Text`
+  color: ${ ({theme}) => theme.colors.shape };
+  font-family: ${ ({theme}) => theme.fonts.secondary_600 };
+  font-size: ${RFValue(15)}px;
+  padding-top: 24px;
+`
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 0 16px;
+`
+
+export const Appointments = styled.View`
+  width: 100%;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 24px 0;
+`
+
+export const AppointmentsTitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${ ( { theme } ) => theme.colors.text};
+  font-family: ${ ( { theme } ) => theme.fonts.primary_400};
+`
+
+export const AppointmentsQuality = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${ ( { theme } ) => theme.colors.title};
+  font-family: ${ ( { theme } ) => theme.fonts.primary_500};
 `
